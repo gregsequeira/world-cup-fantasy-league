e4968c9b65a6d3586605774d45c96e8b48bca26c
gregsequeira <gregorypaulsequeira@gmail.com>
2026-06-01 02:14:51 +0200

    changed port

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 require('./jobs/autoSelect'); // start the auto-selection job
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(cors());
